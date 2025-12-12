694e6086fedca64aca2f0f57b7c3d8b568e8c9bd
lazypandaa <eswarkrishnak2005@email.com>
2025-12-12 20:06:23 +0530

    Fix vite config for dev and production environments

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  base: "/nexksoft/",
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/nexksoft/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
