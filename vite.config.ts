277f48b8deb2695d0947b26330af17a823fb7b19
kgopi122 <gsai4801@gmail.com>
2025-12-16 22:27:42 +0530

    fix: update vite base path to /test/

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/nexksoft/',
+  base: command === 'serve' ? '/' : '/test/',
   plugins: [react()],
   resolve: {
     alias: {
